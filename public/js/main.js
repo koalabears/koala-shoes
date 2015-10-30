8e6a28ab0d1cee29fede5a4d8196c87250f5d3e2
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2015-10-30 12:31:40 +0000

    @hdrdavies broke main.js into two files

--- public/js/main.js
+++ public/js/main.js
@@ -1,159 +1,37 @@
 window.onload = function() {
   main();
-
+  HTMLBuilder = buildHTML();
   function main() {
     var issueData = getIssueData(function(issueData) {
-      buildPageFromIssueData(issueData);
+      HTMLBuilder.buildPageFromIssueData(issueData);
       addEventListeners(issueData);
     });
   }
 
-  function buildPageFromIssueData(issueData) {
-    var contentWrapperDiv = getContentWrapperDiv();
-    var issueDataAsHtml = buildHtmlFromIssueData(issueData);
-    contentWrapperDiv.innerHTML = issueDataAsHtml;
-    return issueData;
-  }
-
-  function getContentWrapperDiv() {
-    return document.getElementsByClassName('wrapper-main')[0];
-  }
-
-  function buildHtmlFromIssueData(issueData) {
-    return issueData.map(buildCollapsedIssueDiv).join('');
-  };
-
-  function buildCollapsedIssueDiv(issue) {
-    var html = '';
-    var unviewedChanges = issue.unviewedIssueChanges;
-    if (unviewedChanges && unviewedChanges.length !==0) {
-      html += '<div class = \'issue active\'>'
-      html += '<h2>' + issue.title + '<\/h2><div class=\'issue-content\'>'
-      html += buildCollapsedIssueContentHTML(issue);
-      html += buildVisibleIssueButtonsHTML();
-    } else {
-      html += '<div class = \'issue\'>'
-      html += '<h2>' + issue.title + '<\/h2><div class=\'issue-content\'>'
-      html += buildCollapsedIssueContentHTML(issue);
-      html += buildHiddenIssueButtonsHTML();
-    }
-    html += '<\/div>';
-    return html;
-  }
-
-  function buildCollapsedIssueContentHTML(issue) {
-    var unviewedChanges = issue.unviewedIssueChanges;
-    var html = '';
-    if (unviewedChanges && unviewedChanges.length !==0) {
-      html += buildHtmlFromUnviewedIssueData(unviewedChanges);
-      html += '<\/div>'
-    } else {
-      html += '<\/div>'
-    }
-    return html;
-  }
-
-  function buildExpandedIssueContentHTML(issue) {
-    var unviewedChanges = issue.unviewedIssueChanges;
-    var viewedChanges = issue.viewedIssueChanges;
-    var html = '';
-    if (unviewedChanges && unviewedChanges.length !==0) {
-        html += buildHtmlFromUnviewedIssueData(unviewedChanges);
-    }
-    if (viewedChanges && viewedChanges.length !==0) {
-        html += buildHtmlFromViewedIssueData(viewedChanges);
-    }
-    return html;
-  }
-
-  function buildHtmlFromUnviewedIssueData(unviewedChanges) {
-    return unviewedChanges.map(function(unviewedChange) {
-      var html = '<div class = \'unviewedChange\'>'
-      if (unviewedChange.type === 'comment') {
-        html += unviewedChange.text;
-      }
-      html += '<\/div>';
-      return html;
-    }).join('');
-  }
-
-  function buildHtmlFromViewedIssueData(viewedChanges) {
-    return viewedChanges.map(function(viewedChange) {
-      var html = '<div class = \'viewedChange\'>'
-      if (viewedChange.type === 'comment') {
-        html += viewedChange.text;
-      }
-      html += '<\/div>';
-      return html;
-    }).join('');
-  }
 
   function addEventListeners(issueData) {
-    var issueDivArray = getIssueDivArray();
+    var issueDivArray = HTMLBuilder.getIssueDivArray();
     issueDivArray.forEach(function(issueDiv, index) {
       addClickToggle(issueDiv, issueData[index]);
       addButtonFunctionality(issueDiv, issueData[index]);
     });
   }
 
-  function getIssueDivArray() {
-    var issueDivs = document.getElementsByClassName('issue');
-    return [].slice.apply(issueDivs);
-  }
-
   function addClickToggle(issueDiv, issueData) {
     issueDiv.getElementsByTagName('h2')[0]
-      .addEventListener('click', function(e) {
+      .addEventListener('click', function(event) {
       var issueContentDiv = issueDiv.getElementsByClassName('issue-content')[0];
-      e.stopPropagation();
+      event.stopPropagation();
       if (hasClass('showHistory', issueDiv)) {
         issueContentDiv.innerHTML = buildCollapsedIssueContentHTML(issueData);
       } else {
         issueContentDiv.innerHTML = buildExpandedIssueContentHTML(issueData);
       }
-      if (!hasClass('active', issueDiv)) {
-        toggleClass('hidden', issueDiv.getElementsByClassName('buttons')[0]);
+      if (!HTMLBuilder.hasClass('active', issueDiv)) {
+        HTMLBuilder.toggleClass('hidden', issueDiv.getElementsByClassName('buttons')[0]);
       }
-      toggleClass('showHistory', issueDiv)
+      HTMLBuilder.toggleClass('showHistory', issueDiv)
     });
-  }
-
-  function hasClass(className, htmlObj) {
-    var re = new RegExp("\\b("+className+")\\b");
-    return re.test(htmlObj.className);
-  }
-
-  function toggleClass(className, htmlObj) {
-    var re = new RegExp("\\b("+className+")\\b");
-    if (hasClass(className, htmlObj)) {
-      htmlObj.className = htmlObj.className.replace(re, '');
-    } else {
-      htmlObj.className += ' ' + className;
-      htmlObj.className = htmlObj.className.replace(/  +/g, ' ');
-    }
-  }
-
-  function buildHiddenIssueButtonsHTML() {
-    var html = '<div class=\'buttons hidden\'>';
-    html += buildButtonsHTML();
-    html += '<\/div>';
-    return html;
-  }
-
-  function buildVisibleIssueButtonsHTML() {
-    var html = '<div class=\'buttons\'>';
-    html += buildButtonsHTML();
-    html += '<\/div>';
-    return html;
-  }
-
-  function buildButtonsHTML() {
-    return buildButton('OK') + buildButton('close') + buildButton('comment');
-  }
-
-
-  function buildButton(name) {
-    return '<button>' + name + '<\/button>'
   }
 
   function addButtonFunctionality(issueDiv, issueData) {
